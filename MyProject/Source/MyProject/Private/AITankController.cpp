f3c32ff569cafad47c3968d14e698c6428a69336
PLAYERNUMBERONE\Harry-Dean <longname3141@gmail.com>
2018-03-12 15:08:32 +1100

    Created the TankAimingComponenet but the cpp is in private and teh .h is in public

--- MyProject/Source/MyProject/Private/AITankController.cpp
+++ MyProject/Source/MyProject/Private/AITankController.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Tank.h"
+
 #include "AITankController.h"
+#include "Tank.h"
 
 void AAITankController::BeginPlay()
 {
